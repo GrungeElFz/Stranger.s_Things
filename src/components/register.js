2e7b5d4c6d9ac4f1cf4cbd94c131e973fe1a424e
GrungeElFz <elf0114_@hotmail.com>
2022-10-05 17:58:21 -0500

    Added: register.js , register function  (src/components/register.js)

---
+++ src/components/register.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { registerUser } from '../api/index.js';
+
+const register = () => {
+    const [newUsername, setNewUsername] = useState('');
+    const [newPasswordConf, setNewPasswordConf] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+
+    const submitUser = async (event) => {
+        event.preventDefault();
+
+        if (newPassword !== newPasswordConf) {
+            alert('The password does not match.');
+
+        } else {
+
+            const result = await registerUser(newUsername, newPassword);
+            const token = result.data.token;
+
+            localStorage.setItem('newUsername', newUsername);
+            localStorage.setItem('token', token);
+
+            setNewUsername('');
+            setNewPassword('');
+            setNewPasswordConf('');
+
+            alert('Successfully Registered.');
+        }
+    } 
+
+}
