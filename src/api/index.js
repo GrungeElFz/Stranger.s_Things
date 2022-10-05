1b3447014f59942ec63a191d65ebf39e944bb0ec
GrungeElFz <elf0114_@hotmail.com>
2022-10-04 19:28:25 -0500

    Added: userLogin function  (.src/api/index.js)

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,22 @@ const BASE_API = `${BASE_URL}/api/${COHORT_NAME}`
 export const getPosts = async () => {
     fetch(`${BASE_API}/posts`)
         .then(response => response.json())
-        .then(result => {
-            console.log(result)
-        })
+        .then(result => {console.log(result)})
         .catch(console.error)
-}
+};
+
+
+export const userLogin = async (username, password) => {
+    const response = await fetch(`${BASE_API}/users/login`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    const result = await response.json();
+
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    localStorage.setItem("username", username);
+    return token;
+
+};
