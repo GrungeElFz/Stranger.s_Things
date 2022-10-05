d015a9736b050bbdf2993dd30f521b171a4d5d9c
GrungeElFz <elf0114_@hotmail.com>
2022-10-05 18:13:46 -0500

    Modified: Added <Register /> in App  (src/index.js)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { useState, useEffect } from 'react';
 import { Posts } from './components'
 import { getPosts } from './api'
-
-// const BASE_URL = "https://strangers-things.herokuapp.com";
-// const COHORT_NAME = "2208-FTB-ET-WEB-FT";
-// const BASE_API = `${BASE_URL}/api/${COHORT_NAME}`;
+import Register from './components/Register';
 
 const App = () => {
-    // const [userPosts, setUserPosts] = useState();
-
     useEffect(() => {
         const getData = async () => {
             await getPosts();
@@ -17,17 +12,13 @@ const App = () => {
         getData();
     }, []);
 
-    // const getPosts = async () => {
-    //     const response = await fetch(`${BASE_API}/posts`)
-    //     const result = await response.json();
-    //     const userPostsList = result.results;
-    //     setUserPosts(userPostsList)
-    // }
-
     return (
-        <Posts />
+        <>
+            <Register />,
+            <Posts />
+        </>
     )
-}
+};
 
 const container = document.getElementById('app');
 const root = createRoot(container);
