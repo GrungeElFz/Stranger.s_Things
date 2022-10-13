481b750c55a03fad8f9e29156269eef6d7b7e2b4
GrungeElFz <elf0114_@hotmail.com>
2022-10-13 10:40:55 -0500

    Fixed: Renamed Register2.js -> Register.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Posts } from './Posts'
 export { default as Login } from './Login'
-export { default as Register } from './Register2'
+export { default as Register } from './Register'
 export { default as Header } from './Header'
 export { default as CreatePost } from './CreatePost'
